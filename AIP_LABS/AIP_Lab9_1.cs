06dd179ffe72542802bb304ecc9e4c238bbbc365
K0teize <ktv110687@gmail.com>
2025-05-20 08:30:55 +0600

    Add files via upload

---
+++ AIP_LABS/AIP_Lab9_1.cs
@@ -0,0 +1,63 @@
+﻿struct Book
+{
+    public string AuthorName {  get;}
+    public string BookName { get;}
+    public string RelDate { get;}
+    public string Publisher { get;}
+    public List<Record> Records { get;}
+    public Book(string authorName, string bookName, string relDate, string publisher,List<Record> records)
+    {
+        AuthorName = authorName;
+        BookName = bookName;
+        RelDate = relDate;
+        Publisher = publisher;
+        Records = new List<Record>(records);
+    }
+}
+class Record
+{
+    public string IssueDate { get; set; }
+    public string LeaseDate { get; set;}
+    public Record(string issueDate, string leaseDate)
+    {
+        IssueDate = issueDate;
+        LeaseDate = leaseDate;
+    }
+}
+class Program
+{
+   
+    static void Main()
+    {
+        List<Book> books = new List<Book>
+        {
+            new Book("Джордж Оруэлл","1984","1949","АСТ", new List<Record> {new Record("12.01.2000","15.03.2000"),new Record("04.05.2004","09.06.2004")}),
+            new Book("Дж. Р. Р. Толкин","Властелин Колец","1954-1955","АСТ", new List<Record>{new Record("22.07.2014",null)}),
+            new Book("Агата Кристи","Убийство в Восточном Экспрессе","1934","Эксмо",new List<Record>{new Record(null,null)}),
+            new Book("Том Харрис","Молчание ягнят","1988","АСТ", new List<Record>{new Record("03.02.2004","05.04.2004"),new Record("07.07.2006",null)})
+        };
+        Console.WriteLine("Книги которые не выдавались\n");
+        foreach (Book book in books)
+        {
+            foreach(Record record in book.Records)
+            {
+                if (record.IssueDate == null && record.LeaseDate == null)
+                {                    
+                    Console.WriteLine($"Название книги: {book.BookName}\nАвтор: {book.AuthorName}\nДата выхода книги: {book.RelDate}\nИздательство: {book.Publisher}\n");
+                }
+            }
+        }
+        Console.WriteLine("Книги которые не были сданы\n");
+        foreach(Book book in books)
+        {
+            foreach (Record record in book.Records)
+            {
+                if(record.IssueDate != null && record.LeaseDate == null)
+                {
+                    Console.WriteLine($"Название книги: {book.BookName}\nАвтор: {book.AuthorName}\nДата выхода книги: {book.RelDate}\nИздательство: {book.Publisher}\n");
+                }
+            }
+        }
+        
+    }
+}
---
+++ AIP_LABS/AIP_Lab9_2.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string[] text = File.ReadAllLines($"C:\\Users\\ktv11\\Desktop\\file.txt");
+        List<string> list = new List<string>();
+        foreach( string line in text )
+        {
+            if(line.Any(s => char.IsDigit(s) && (s - '0') % 2 != 0))
+            {
+                list.Add(line);
+            }
+        }
+        File.WriteAllLines($"C:\\Users\\ktv11\\Desktop\\output.txt", list);
+    }
+}
